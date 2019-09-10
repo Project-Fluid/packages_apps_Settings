a1503cead7f64dbf1bab5794ef37401f74a6bc0e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 13:15:06 -0300

    Revert "FeatureFactoryImpl: Implement SuggestionFeatureProvider"
    This reverts commit ce6f6d3d001cdb1eb3e24f1772c2feeeb12b983a.

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,18 +1,13 @@
 package com.google.android.settings.overlay;
 
-import android.content.Context;
-
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.android.settings.search.SearchFeatureProvider;
-import com.android.settings.dashboard.suggestions.SuggestionFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
-import com.google.android.settings.dashboard.suggestions.SuggestionFeatureProviderGoogleImpl;
 import com.google.android.settings.search.SearchFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
     private AccountFeatureProvider mAccountFeatureProvider;
     private SearchFeatureProvider mSearchFeatureProvider;
-    private SuggestionFeatureProvider mSuggestionFeatureProvider;
 
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
@@ -28,13 +23,5 @@ public final class FeatureFactoryImpl extends com.android.settings.overlay.Featu
             mSearchFeatureProvider = new SearchFeatureProviderGoogleImpl();
         }
         return mSearchFeatureProvider;
-    }
-
-    @Override
-    public SuggestionFeatureProvider getSuggestionFeatureProvider(Context context) {
-        if (mSuggestionFeatureProvider == null) {
-            mSuggestionFeatureProvider = new SuggestionFeatureProviderGoogleImpl(context.getApplicationContext());
-        }
-        return mSuggestionFeatureProvider;
     }
 }
