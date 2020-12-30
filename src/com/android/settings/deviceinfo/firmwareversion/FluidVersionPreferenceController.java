14bafe941dc1eb7f40ee1fa9c8769f9b0f0437d6
elluzion <dyrex2004@gmail.com>
2020-12-31 00:10:03 +0100

    Settings: Redesign Fluid Version
    What we do here is introduce our new Layout as a LayoutPreference which is a part of AOSP Settings. Therefore we nuke the preferences related to the Fluid Version and Specs, use the FluidVersionPreferenceController for the new Layout and use the SpecsPreferenceController as a new Util which provides the static code for device-related info such as RAM, battery capacity, screen specifications etc.
    
    Add this to your device tree:
    
    PRODUCT_PRODUCT_PROPERTIES += \
      ro.fluid.maintainer=maintainername \
      ro.fluid.cpu=for example SDM855
    
    Co-authored-by: Androbuddy <anirudhchimpidi8@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/FluidVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FluidVersionPreferenceController.java
@@ -15,12 +15,21 @@
  */
 package com.android.settings.deviceinfo.firmwareversion;
 
+import java.io.IOException;
 import android.content.Context;
 import android.os.SystemProperties;
 import androidx.annotation.VisibleForTesting;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
+import com.android.settings.utils.FluidSpecUtils;
 import com.android.settings.core.BasePreferenceController;
+import com.android.settingslib.widget.LayoutPreference;
+import com.android.settingslib.Utils;
+import com.android.settings.core.PreferenceControllerMixin;
+
+import android.widget.TextView;
 
 public class FluidVersionPreferenceController extends BasePreferenceController {
 
@@ -28,8 +37,30 @@ public class FluidVersionPreferenceController extends BasePreferenceController {
     private static final String FLUID_VERSION_PROPERTY = "ro.fluid.build.version";
     private static final String FLUID_BUILD_TYPE = "ro.fluid.buildtype";
 
+    @VisibleForTesting
+    TextView mFluidVersionText;
+    @VisibleForTesting
+    TextView mFluidVersionFlavourText;
+    @VisibleForTesting
+    TextView mDeviceNameText;
+    @VisibleForTesting
+    TextView mCpuText;
+    @VisibleForTesting
+    TextView mScreenResText;
+    @VisibleForTesting
+    TextView mBatteryText;
+    @VisibleForTesting
+    TextView mRamText;
+    @VisibleForTesting
+    TextView mMaintainerText;
+
+    private PreferenceFragmentCompat mHost;
+    private LayoutPreference mFluidVersionLayoutPref;
+    private Context mContext;
+
     public FluidVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        mContext = context;
     }
 
     @Override
@@ -37,14 +68,73 @@ public class FluidVersionPreferenceController extends BasePreferenceController {
         return AVAILABLE;
     }
 
-    @Override
-    public CharSequence getSummary() {
-     String fluidVer =  SystemProperties.get(FLUID_VERSION_PROPERTY);
-     String fluidType = SystemProperties.get(FLUID_BUILD_TYPE);
-
-      if (!fluidVer.isEmpty() && !fluidType.isEmpty())
-	    return fluidVer + " /" + "/ " + fluidType;
-	else
-            return mContext.getString(R.string.unknown);
+    public void setFragment(PreferenceFragmentCompat fragment) {
+        mHost = fragment;
     }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mFluidVersionLayoutPref = screen.findPreference(getPreferenceKey());
+        mFluidVersionText = mFluidVersionLayoutPref.findViewById(R.id.fluid_version);
+        mFluidVersionFlavourText = mFluidVersionLayoutPref.findViewById(R.id.fluid_version_flavour);
+        mDeviceNameText = mFluidVersionLayoutPref.findViewById(R.id.device_name_text);
+        mCpuText = mFluidVersionLayoutPref.findViewById(R.id.device_cpu_text);
+        mScreenResText = mFluidVersionLayoutPref.findViewById(R.id.device_screen_res_text);
+        mBatteryText = mFluidVersionLayoutPref.findViewById(R.id.device_battery_text);
+        mRamText = mFluidVersionLayoutPref.findViewById(R.id.device_ram_text);
+        mMaintainerText = mFluidVersionLayoutPref.findViewById(R.id.device_maintainer_text);
+
+        UpdateFluidVersionPreference();
+    }
+
+    private void UpdateFluidVersionPreference() {
+        // We split the different specs into different voids to make the code more organized.
+        updateFluidVersionText();
+        updateDeviceNameText();
+        updateCpuText();
+        updateScreenResText();
+        updateBatteryText();
+        updateRamText();
+        updateMaintainerText();
+    }
+
+    private void updateFluidVersionText() {
+        String fluidVer = SystemProperties.get(FLUID_VERSION_PROPERTY);
+        String fluidType = SystemProperties.get(FLUID_BUILD_TYPE);
+        String fluidTypeCapitalized = fluidType.substring(0, 1).toUpperCase() + fluidType.substring(1).toLowerCase();
+        String[] fluidVerSeparated = fluidVer.split("-");
+
+        if (!fluidVer.isEmpty() && !fluidType.isEmpty()) {
+            mFluidVersionText.setText(fluidVerSeparated[0] + " " + fluidTypeCapitalized + " | ");
+            mFluidVersionFlavourText.setText(fluidVerSeparated[1]);
+        } else {
+            mFluidVersionText.setText("");
+            mFluidVersionFlavourText.setText(R.string.unknown);
+        }
+    }
+
+    private void updateDeviceNameText() {
+        mDeviceNameText.setText(FluidSpecUtils.getDeviceName());
+    }
+
+    private void updateBatteryText() {
+        mBatteryText.setText(FluidSpecUtils.getBatteryCapacity(mContext) + " mAh");
+    }
+
+    private void updateCpuText() {
+        mCpuText.setText(FluidSpecUtils.getProcessorModel());
+    }
+
+    private void updateScreenResText() {
+        mScreenResText.setText(FluidSpecUtils.getScreenRes(mContext));
+    }
+
+    private void updateRamText() {
+        mRamText.setText(String.valueOf(FluidSpecUtils.getTotalRAM())+ " GB");
+    }
+    private void updateMaintainerText() {
+        mMaintainerText.setText(String.valueOf(FluidSpecUtils.getMaintainerName()));
+    }
+
 }
