c010058923cfc5f2722351a3393ba54a76badbb9
elluzion <dyrex2004@gmail.com>
2020-12-30 19:41:49 +0100

    Settings: Correct Utils change

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -1207,7 +1207,7 @@ public final class Utils extends com.android.settingslib.Utils {
             }
         }
         return isMediaOutputDisabled;
-
+    }
     public static String normalizeTitleCaseIfRequired(Context context, String input) {
         if (!context.getResources().getBoolean(R.bool.language_capitalizes_nouns)) {
             return input.toLowerCase();
