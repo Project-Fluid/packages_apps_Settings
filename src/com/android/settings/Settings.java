ddc64f353f119d92c91cacfb0ad3a2568499c8e6
maxwen <max.weninger@gmail.com>
2021-10-16 19:57:36 +0000

    Settings: Integrate Fluid Customisation
      * Add it on dashboard
      * Elluzion: Create FluidCustomisation symlink, adapt to 12
    
    Change-Id: I6a4f681183dd8edcf626b774a68c4cd653b414c4

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -324,4 +324,10 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    /**
+     * Fluid Customisation Activity
+     */
+    public static class FluidCustomisationActivity extends SettingsActivity {}
+
 }
